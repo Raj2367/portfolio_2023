997a55f76f877dc60a953b27728228e009aa6efd
pruthwirajsimplilearn <pruthwiraj.nayak@simplilearn.net>
2023-03-06 11:35:39 +0530

    article section added

--- script/index.js
+++ script/index.js
@@ -4,4 +4,13 @@ function darkMode() {
     var button = document.getElementById("db");
     button.classList.toggle("fa-sun");
     button.classList.toggle("fa-moon");
+    var articleContent = document.getElementById("article1");
+    articleContent.classList.toggle("article");
+    articleContent.classList.toggle("article-darkmode");
+    articleContent = document.getElementById("article2");
+    articleContent.classList.toggle("article");
+    articleContent.classList.toggle("article-darkmode");
+    articleContent = document.getElementById("article3");
+    articleContent.classList.toggle("article");
+    articleContent.classList.toggle("article-darkmode");
 }
